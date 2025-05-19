0e2cfc95742384a1f9a0c116105ca335882e75d0
Byenio <marcel.bienko@racing.polsl.pl>
2025-05-19 13:46:15 +0200

    TCCDA-10 feat: create data and packet models

---
+++ EngineerDashboard.Telemetry/Packets/EventPacket.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+using EngineerDashboard.Telemetry.Data;
+
+namespace EngineerDashboard.Telemetry.Packets;
+
+/// <summary>
+/// This packet gives details of events that happen during the course of a session.
+/// </summary>
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct EventPacket
+{
+    /// <summary>
+    /// Packet header
+    /// </summary>
+    public PacketHeader header;
+    /// <summary>
+    /// Event string code
+    /// </summary>
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+    public char[] eventStringCode;
+    /// <summary>
+    /// Event details. Should be parsed differently for each event code.
+    /// </summary>
+    public EventDetails eventDetails;
+}
---
+++ EngineerDashboard.Telemetry/Packets/FinalClassificationPacket.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using EngineerDashboard.Telemetry.Data;
+
+namespace EngineerDashboard.Telemetry.Packets;
+
+/// <summary>
+/// his packet details the final classification at the end of the race, and the data will match with the post race results screen.
+/// This is especially useful for multiplayer games where it is not always possible to send lap times on the final frame because of network delay.
+/// </summary>
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct FinalClassificationPacket
+{
+    /// <summary>
+    /// Packet header
+    /// </summary>
+    public PacketHeader header;
+    /// <summary>
+    /// Number of cars in the final classification
+    /// </summary>
+    public byte numCars;
+    /// <summary>
+    /// List of cars
+    /// </summary>
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 22)]
+    public FinalClassificationData[] classificationData;
+}
