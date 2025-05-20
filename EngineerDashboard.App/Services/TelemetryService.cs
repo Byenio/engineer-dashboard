30c114deca706a13730f5e4b1a799d286bf0e036
Byenio <marcel.bienko@racing.polsl.pl>
2025-05-20 10:11:02 +0200

    TCCDA-12 feat: create desktop app and connect telemetry client

---
+++ EngineerDashboard.App/Services/TelemetryService.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Threading;
+using EngineerDashboard.Telemetry;
+using EngineerDashboard.Telemetry.Packets;
+
+namespace EngineerDashboard.App.Services;
+
+public class TelemetryService : IDisposable
+{
+   public TelemetryClient TelemetryClient { get; } = new TelemetryClient(20777);
+
+   public void Dispose()
+   {
+      TelemetryClient.Stop();
+   }
+}
